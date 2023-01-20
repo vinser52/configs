ec11f724000b5d26b23cf5dee69173c3c486fc04
Sergei Vinogradov <vinser52@gmail.com>
2023-01-20 22:40:13 +0100

    Add install script for Chrome

---
+++ install_chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmour -o /usr/share/keyrings/google_linux_signing_key.g
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google_linux_signing_key.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+
+apt update
+
+apt install google-chrome-stable
