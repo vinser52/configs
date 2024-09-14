f25b754114a7ac53f0ce5340008d3db75a416393
Sergei Vinogradov <vinser52@gmail.com>
2024-09-15 01:05:43 +0200

    Add plugins to install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -12,3 +12,12 @@ sudo apt-get install fonts-powerline
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 sed -i 's/ZSH_THEME="agnoster"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/esc/conda-zsh-completion ~/.oh-my-zsh/custom/plugins/conda-zsh-completion
+
+sed -i 's/^plugins=(\(.*\))/plugins=(\1 colored-man-pages zsh-autosuggestions zsh-syntax-highlighting conda-zsh-completion)/' ~/.zshrc
+
